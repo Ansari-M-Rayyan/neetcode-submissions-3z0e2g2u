f7601f4f000dd7802ff4e2635c9cf68ae18ce76f
Ansari Rayyan <120328367+Ansari-M-Rayyan@users.noreply.github.com>
2026-04-07 12:39:05 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def heapify(n,i):
+            largest =i
+            l = 2*i+1
+            r = 2*i+2
+
+            if l<n and nums[l]>nums[largest]:
+                largest=l
+            if r<n and nums[r]>nums[largest]:
+                largest=r
+            
+            if largest!=i:
+                nums[i] ,nums[largest] = nums[largest] ,nums[i]
+                heapify(n,largest)
+        n=len(nums)
+        for i in range(n//2-1,-1,-1):
+            heapify(n,i)
+        for i in range(n-1,0,-1):
+            nums[i] ,nums[0] = nums[0] ,nums[i]
+            heapify(i,0)
+        return nums
+nums = [5,2,3,1]
+obj=Solution()
+soln=obj.sortArray(nums)
+print(soln)
