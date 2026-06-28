70a2ac3c3b83e74152adab693fd781e0c3e051c8
Ansari Rayyan <120328367+Ansari-M-Rayyan@users.noreply.github.com>
2026-06-28 10:02:52 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        for i in nums:
+            hashmap[i] = hashmap.get(i , 0) + 1 # {1: 3, 2: 2, 100: 1}
+        sorted_dict = dict(sorted(hashmap.items() , key = lambda x:x[1] , reverse = True))
+        return list(sorted_dict.keys())[:k]
+
+nums = [1,1,1,2,2,100]
+k=2
+obj=Solution()
+soln=obj.topKFrequent(nums,k)
+print(soln)
+
+'''
+
+Time Complexity : O(nlogn)
+Space Complexity : O(n) for hashmap
+
+hashmap.items(): Dictionary ko pairs mein tod deta hai -> [(1, 3), (2, 2), (100, 1)]. 
+Yahan har tuple hai (number, uski_frequency).
+lambda x: x[1]: Yeh sorting ka rule hai. Yeh kehta hai ki tuple x ke index 1 (yani frequency) ke hisaab se sort karo. 
+Agar x[0] likhte toh woh keys (number) ke hisaab se sort karta.
+reverse = True: Bade numbers (highest frequency) pehle aayenge (Descending order).
+dict(...): Wapas us list ko dictionary bana deta hai.
+Resulting sorted_dict: {1: 3, 2: 2, 100: 1} (Highest count pehle).
+
+'''
